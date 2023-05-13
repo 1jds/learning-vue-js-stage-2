1ac3decc4a794f34963b5cabe1eaadac02e730ab
Your Name <jamesdstacey2@gmail.com>
2023-05-13 15:14:12 +1000

    Added the dev server

--- vue-practice-project/vite.config.js
+++ vue-practice-project/vite.config.js
@@ -5,11 +5,30 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [vue(), vueJsx()],
+//   resolve: {
+//     alias: {
+//       '@': fileURLToPath(new URL('./src', import.meta.url))
+//     }
+//   }
+// })
+
+
 export default defineConfig({
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '/'),
+      },
+    },
+  },
 })
