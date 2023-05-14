4120bc27468cf2ec7999dc603c3ee084486faa24
Your Name <jamesdstacey2@gmail.com>
2023-05-14 22:18:35 +1000

    Added aync requests

--- vue-practice-project/src/components/AddTask.vue
+++ vue-practice-project/src/components/AddTask.vue
@@ -1,22 +1,3 @@
-<template>
-    <form @submit="onSubmit" class="add-form">
-        <div class="form-control">
-            <label>Task</label>
-            <input type="text" v-model="text" name="text" placeholder="Add Task" />
-        </div>
-        <div class="form-control">
-            <label>Day & Time</label>
-            <input type="text" v-model="day" name="day" placeholder="Add Day & Time" />
-        </div>
-        <div class="form-control form-control-check">
-            <label>Set Reminder</label>
-            <input type="checkbox" v-model="reminder" name="reminder" />
-        </div>
-
-        <input type="submit" value="Save Task" class="btn btn-block" />
-    </form>
-</template>
-
 <script>
 export default {
     name: 'AddTask',
@@ -37,7 +18,6 @@ export default {
             }
 
             const newTask = {
-                id: Math.floor(Math.random() * 100000),
                 text: this.text,
                 day: this.day,
                 reminder: this.reminder,
@@ -51,6 +31,25 @@ export default {
     },
 }
 </script>
+
+<template>
+    <form @submit="onSubmit" class="add-form">
+        <div class="form-control">
+            <label>Task</label>
+            <input type="text" v-model="text" name="text" placeholder="Add Task" />
+        </div>
+        <div class="form-control">
+            <label>Day & Time</label>
+            <input type="text" v-model="day" name="day" placeholder="Add Day & Time" />
+        </div>
+        <div class="form-control form-control-check">
+            <label>Set Reminder</label>
+            <input type="checkbox" v-model="reminder" name="reminder" />
+        </div>
+
+        <input type="submit" value="Save Task" class="btn btn-block" />
+    </form>
+</template>
 
 <style scoped>
 .add-form {
